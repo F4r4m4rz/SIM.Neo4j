78088b21fadebbbf18715d315da90e168ca1c784
Faramarz Bodaghi <faramarz.bodaghi@outlook.com>
2020-07-16 22:46:47 +0200

    Added SimEntity

---
+++ SIM.Neo4j/Abstractions/SimEntity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SIM.Neo4j.Abstractions
+{
+    /// <summary>
+    /// Represents an abstraction of graph entities
+    /// </summary>
+    public abstract class SimEntity
+    {
+        /// <summary>
+        /// id value of the entity in neo4j database
+        /// </summary>
+        public int Neo4jId { get; set; }
+
+        /// <summary>
+        /// A dictionary where all properties are stored
+        /// <p>Key: parameter name</p>
+        /// <p>Value: parameter value</p>
+        /// </summary>
+        public IDictionary<string, object> Properties { get; set; }
+    }
+}
